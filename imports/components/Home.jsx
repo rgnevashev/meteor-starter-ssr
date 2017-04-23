3c49f0a29c58c141fb7edf9ca91a3692b305f2da
Roman Karmatsky <rgnevashev@gmail.com>
2017-04-23 07:19:56 -0500

    scripts

--- imports/components/Home.jsx
+++ imports/components/Home.jsx
@@ -1,4 +1,8 @@
 import React from 'react'
+//import { Button } from 'react-bootstrap' // 259kb
+//import BootstrapTable from 'react-bootstrap-table/lib/BootstrapTable' // 500kb | 467kb from lib
+//import moment from 'moment' // 52kb
+
 import alertify from '/imports/lib/alertify'
 
 const Home = (props) => (
